df960964fd17652f13dea12db894c8f6a6011fc5
mrlukasbos <mrlukasbos@gmail.com>
2019-06-06 11:15:42 +0200

    possible fix for grsim geneva bug

--- src/GRSim.cpp
+++ src/GRSim.cpp
@@ -332,7 +332,7 @@ void addRobotCommandToPacket(grSim_Packet& packet, roboteam_msgs::RobotCommand c
     float angles[] = {20, 10, 0, -10, -20};
 
     // geneva_angle in radians
-    float geneva_angle = 2*M_PI*angles[msg.geneva_state-1]/360;
+    float geneva_angle = 2*M_PI*angles[genevaState]/360;
 
     command->set_geneva_angle(geneva_angle);
 }
